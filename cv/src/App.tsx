c8c68cb6d4c1b5c9d965b49e9b9eb64e271b48b8
Adam Watchmaker <watchmaker1337@gmail.com>
2025-01-10 17:17:05 +0100

    add jsx elements

--- cv/src/App.tsx
+++ cv/src/App.tsx
@@ -15,11 +15,57 @@ function App() {
     experience: Experience[];
     education: string[];
   }
-
+  
+  const cvData: CvData = { 
+    photo: "../public/avatar.jpg",
+    name: 'Adam',
+    lastName: 'Zegarek',
+    experience: [{
+      year: 2017,
+      description:'Wyjazd zarobkowy do Holandi'
+    },
+    {
+      year:2018,
+      description: 'Wyjazd zarobkowy do Niemiec'
+    },
+    {
+      year: 2019,
+      description: 'Praca jako pomocnik stolarza'
+    }
+  ],
+    education:['Szkoła średnia','Kurs Angielskiego na Etutor','Kurs Programowania MegaK'],  
+  }
 
   return (
     <>
-     <h1>Begin</h1>
+     <div className='container'>
+      <h1>CV</h1>
+      <div className='img__container'>
+        <img src={cvData.photo} alt="" />
+      </div>
+      <div className='info__container'>
+        Imię: {cvData.name}
+        Nazwisko: {cvData.lastName}
+      </div>
+      <ul className='exp__list'>
+        <h2>Doświadczenie zawodowe</h2>
+        {cvData.experience.map((exp)=>{
+          
+          return <li className='exp__item'> 
+            <span className='exp__year'> * {exp.year}</span> - {exp.description} 
+          </li>
+        })}
+      </ul>
+      <ul className='edu__list'>
+        <h2>Doświadczenie zawodowe</h2>
+        {cvData.education.map((edu)=>{
+          
+          return <li className='edu__item'> 
+            * {edu}
+          </li>
+        })}
+      </ul>
+     </div>
     </>
   )
 }
